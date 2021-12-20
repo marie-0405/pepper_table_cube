a8921ca5eb506f202f6552ead3b179c4d3c086bb
marie-0405 <sirisuamari0405@yahoo.co.jp>
2021-12-20 16:40:31 +0900

    Add actionでPepperを動かすためのテストファイル(action_pepper_move.py)

---
+++ src/test/pepper_move.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import os
+from subprocess import check_call
+
+import actionlib_msgs.msg
+from geometry_msgs.msg import Pose, PoseStamped, Point, Quaternion
+from moveit_commander import MoveGroupCommander, conversions
+from rospkg import RosPack
+import roslib
+import rospy
+import std_msgs.msg
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from tf.transformations import quaternion_from_euler
+import time
+
+rospy.init_node("test_pepper_moveit")
+
+move_group = "right_arm"
+time.sleep(5)
+arm = MoveGroupCommander(move_group)
+print arm.get_current_pose().pose
+running_pub = rospy.Publisher("/pepper_dcm/RightArm_controller/command", JointTrajectory)
+cancel_pub = rospy.Publisher("/move_group/cancel", actionlib_msgs.msg.GoalID)
+
+
+def demo() :
+  for p in [[ 0.35, 0.35, 0.1],
+            [ 0.3,  0.2, 0.1],]:
+    print "set_pose_target(", p, ")"
+    pose = PoseStamped(header = rospy.Header(stamp = rospy.Time.now(), frame_id = '/torso'),
+                        pose = Pose(position = Point(*p),
+                        orientation = Quaternion(*quaternion_from_euler(1.57, 0, 1.57, 'sxyz'))))
+    print "pose", pose
+    arm.set_pose_target(pose)
+    arm.go() or arm.go() or rospy.logerr("arm.go fails")
+    rospy.sleep(1)
+    if rospy.is_shutdown():
+        return
+
+if __name__ == "__main__":
+    while not rospy.is_shutdown():
+        demo()
