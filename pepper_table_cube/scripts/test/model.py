769bad14204ce74e700ab5d3af4dac484fce3907
marie-0405 <sirisuamari0405@yahoo.co.jp>
2022-02-10 18:27:14 +0900

    Use wait_for_message to get model states

--- pepper_table_cube/scripts/test/model.py
+++ pepper_table_cube/scripts/test/model.py
@@ -3,22 +3,29 @@
 from gazebo_msgs.msg import ModelStates
 from geometry_msgs.msg import Pose, Quaternion, Twist, Vector3
 import rospy
+import time
 
 class Model():
   def __init__(self, name):
-    self.name = name
     rospy.init_node('model_state_sub', anonymous=True)
-    self.sub = rospy.Subscriber('/gazebo/model_states', ModelStates, self.get_position )
-    rospy.spin()
-    
-  def get_position(self, msg):
-    index = msg.name.index(self.name)
-    position = ModelStates()
-    print(msg.pose[index])
-    return msg.pose[index]
+
+    self.name = name
+    self.topic = '/gazebo/model_states'
+
+    msg = rospy.wait_for_message(self.topic, ModelStates)
+    self.index = msg.name.index(self.name)
+  
+  def get_position(self):
+    msg = rospy.wait_for_message(self.topic, ModelStates)
+    return msg.pose[self.index]
+  
+  def get_twist(self, msg):
+    msg = rospy.wait_for_message(self.topic, ModelStates)
+    return msg.twist[self.index]
 
 if __name__ == '__main__':   
   try:
     cube = Model("cube")
+    print(cube.get_position())
   except rospy.ROSInterruptException: pass
   
