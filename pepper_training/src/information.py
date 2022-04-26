4e8d0a2685960953ef4ca53aeb1a46464cdfe08c
marie-0405 <sirisuamari0405@yahoo.co.jp>
2022-04-26 16:29:35 +0900

    add plot result information function Informationクラスを用いて、出力情報を表示

--- pepper_training/src/information.py
+++ pepper_training/src/information.py
@@ -1,20 +1,34 @@
 # coding: UTF-8
+import matplotlib.pyplot as plt
 import pandas as pd
 import rospkg
 import rospy
 
 class Information(object):
 
-  def __init__(self, **kwargs):
-    rospy.loginfo(kwargs)
-    self.df = pd.DataFrame(kwargs)
-  
-  def write(self, file_name):
-    # Get the file path
+  def __init__(self, file_name, **kwargs):
+    # Set the file path ファイルパスを設定
     rospack = rospkg.RosPack()
     pkg_path = rospack.get_path('pepper_training')
-    file_path = pkg_path + '/training_results/' + file_name
+    self.file_path = pkg_path + '/training_results/' + file_name
 
+    if kwargs:
+      self.df = pd.DataFrame(kwargs)
+  
+  def write(self):
     # output dataframe to csv
-    self.df.to_csv(file_path)
+    self.df.to_csv(self.file_path)
+
+  def read(self):
+    self.df = pd.read_csv(self.file_path)
+
+  def plot(self, column):
+    """
+    param
+      - column:string column name you want to plot
+    """
+    self.read()
+    self.df[column].plot(figsize=(11, 6))
+    plt.show()
+
 
--- pepper_training/src/show_rewards.py
+++ pepper_training/src/show_rewards.py
@@ -1,23 +1,11 @@
 #!/usr/bin/env python
 
-import json
-import matplotlib.pyplot as plt
-import numpy as np
-import rospkg
+import sys
 
-rewards = []
+from information import Information
 
-# Set the logging system
-rospack = rospkg.RosPack()
-pkg_path = rospack.get_path('pepper_training')
-readfile = pkg_path + '/training_results/rewards.txt'
+file_name = sys.argv[1] if len(sys.argv)==2 else 'rewards.csv'
 
-with open(readfile, 'r') as f:
-  for line in f:
-    rewards.append(float(line.rstrip('\n')))
-nepisodes = np.arange(1, len(rewards) + 1)
-
-plt.figure()
-plt.plot(nepisodes, rewards)
-plt.show()
+information = Information(file_name)
+information.plot('reward')
 
