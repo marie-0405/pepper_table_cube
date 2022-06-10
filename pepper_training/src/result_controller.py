3d9a82a1f30b6baae3d759d9d833a4f7ea83c7dd
marie-0405 <sirisuamari0405@yahoo.co.jp>
2022-06-10 15:50:55 +0900

    [add] counter of the number of Q matrix Qmatrix の状態と行動の組み合わせのカウンターを追加

--- pepper_training/src/result_controller.py
+++ pepper_training/src/result_controller.py
@@ -1,4 +1,5 @@
 # coding: UTF-8
+from ast import literal_eval
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -13,18 +14,24 @@ class ResultController():
     rospack = rospkg.RosPack()
     pkg_path = rospack.get_path('pepper_training')
     self.reward_file_path = pkg_path + '/training_results/reward-'+ file_name_end + '.csv'
-    self.matrix_file_path = pkg_path + '/training_results/q_matrix-'+ file_name_end + '.txt'
+    self.q_matrix_file_path = pkg_path + '/training_results/q_matrix-'+ file_name_end + '.txt'
 
   def write(self, rewards, succeeds, q_matrix):
     """Output result dataframe to csv"""
     result = Result(rewards, succeeds, q_matrix)
     result.df.to_csv(self.reward_file_path)
-    with open(self.matrix_file_path, "w") as f:
+    with open(self.q_matrix_file_path, "w") as f:
       f.write(str(result.q_matrix))
   
   def _read(self):
     result_df = pd.read_csv(self.reward_file_path, engine="python")
     return result_df
+
+  def count_q_matrix(self):
+    q_matrix = {}
+    with open(self.q_matrix_file_path) as f:
+      lines = f.read()
+      return lines.count(':')
   
   def plot_reward(self):
     result_df = self._read()
@@ -41,7 +48,7 @@ class ResultController():
     plt.xlabel("The number of episode")
     plt.ylabel("Reward")
 
-    plt.ylim([-13.0, -3.0])
+    plt.ylim([-25.0, -0.0])
     plt.legend()
     plt.show()
 
