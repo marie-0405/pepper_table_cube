1c0203bbea2cd09806180736712f3b9b98310ac3
marie-0405 <sirisuamari0405@yahoo.co.jp>
2022-09-16 01:29:31 -0700

    [add] tuning hyper parameters using Ray2

--- src/pepper_training/src/actor.py
+++ src/pepper_training/src/actor.py
@@ -11,13 +11,13 @@ class Actor(nn.Module):
     (linear3): Linear(in_features=256, out_features=2, bias=True)
   )
   """
-  def __init__(self, state_size, action_size):
+  def __init__(self, state_size, action_size, L1, L2):
     super(Actor, self).__init__()
     self.state_size = state_size
     self.action_size = action_size
-    self.linear1 = nn.Linear(self.state_size, 128)
-    self.linear2 = nn.Linear(128, 256)
-    self.linear3 = nn.Linear(256, self.action_size)
+    self.linear1 = nn.Linear(self.state_size, L1)
+    self.linear2 = nn.Linear(L1, L2)
+    self.linear3 = nn.Linear(L2, self.action_size)
 
   def forward(self, state):
     output = F.relu(self.linear1(state))
--- src/pepper_training/src/critic.py
+++ src/pepper_training/src/critic.py
@@ -4,13 +4,13 @@ import torch.nn.functional as F
 
 
 class Critic(nn.Module):
-  def __init__(self, state_size, action_size):
+  def __init__(self, state_size, action_size, L1, L2):
     super(Critic, self).__init__()
     self.state_size = state_size
     self.action_size = action_size
-    self.linear1 = nn.Linear(self.state_size, 128)
-    self.linear2 = nn.Linear(128, 256)
-    self.linear3 = nn.Linear(256, 1)
+    self.linear1 = nn.Linear(self.state_size, L1)
+    self.linear2 = nn.Linear(L1, L2)
+    self.linear3 = nn.Linear(L2, 1)
 
   def forward(self, state):
     output = F.relu(self.linear1(state))
